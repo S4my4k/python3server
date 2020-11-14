bc31ae6037fb089888a10290ba4451634758d8d1
System Exploited <59100756+SystemExploited@users.noreply.github.com>
2020-11-14 00:17:53 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
-
 server_address = ('0.0.0.0', 8081)    
 httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
-os.chdir('C:/')  # optional
-print('Running server...')
+print('The Server is Running...')
 httpd.serve_forever()
